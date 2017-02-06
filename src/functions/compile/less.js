f7d8d257a003eea8b679bed4415b2ac01b319fc8
Zachary <admin@hackzzila.com>
2017-02-05 20:25:04 -0600

    less importer

--- src/functions/compile/less.js
+++ src/functions/compile/less.js
@@ -4,13 +4,15 @@ const less = require('less');
 const log = require('npmlog');
 const fs = require('fs-extra');
 const lockfile = require('proper-lockfile');
+const importer = require('../../importers/less');
 
 log.heading = 'dia';
 
 module.exports = filename => new Promise((resolve) => {
   lockfile.unlockSync('./diamond/.internal/packages.lock');
 
-  less.render(fs.readFileSync(filename).toString(), { filename }).then((result) => {
+  less.render(fs.readFileSync(filename).toString(), { filename, plugins: [importer] })
+  .then((result) => {
     resolve(result.css.toString());
   }).catch((error) => {
     log.error('less', error.message);
