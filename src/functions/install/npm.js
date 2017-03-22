83fcafb7c14d263f01574bf13f47d4b1f3aff698
Zachary <admin@hackzzila.com>
2017-03-22 16:32:23 -0400

    fix critical bug

--- src/functions/install/npm.js
+++ src/functions/install/npm.js
@@ -1,62 +1,47 @@
 'use strict';
 
-const Registry = require('npm-registry-client');
 const userAgent = require('../../misc/userAgent');
+const superagent = require('superagent');
 const semver = require('semver');
 const log = require('npmlog');
 
-function noop() { }
-
-const registry = new Registry({
-  userAgent: userAgent.registry,
-  log: {
-    error: log.error,
-    warn: log.warn,
-    info: noop,
-    verbose: noop,
-    silly: noop,
-    http: noop,
-    pause: noop,
-    resume: noop,
-  },
-});
-
 module.exports = (packages, pkg) => new Promise((resolve) => {
-  registry.get(`https://registry.npmjs.org/${pkg.name}`, {}, (err, data) => {
-    if (err) {
-      log.disableProgress();
-      log.resume();
-      if (err.message) log.error('registry', err.message);
-      else throw err;
-      log.error('not ok');
-      process.exit(1);
-    }
-
-    let version;
-    if (pkg.source.tag && data['dist-tags'][pkg.source.tag] && data.versions[data['dist-tags'][pkg.source.tag]]) {
-      version = data.versions[data['dist-tags'][pkg.source.tag]];
-    } else if (pkg.source.tag) {
-      log.disableProgress();
-      log.resume();
-      log.error(`invalid tag ${pkg.source.tag}`, pkg.name);
-      log.error('not ok');
-      process.exit(1);
-    } else {
-      const versions = Object.keys(data.versions)
-        .filter(v => semver.satisfies(v, pkg.version))
-        .sort(semver.compare)
-        .reverse();
-      if (versions.length) {
-        version = data.versions[versions[0]];
-      } else {
+  superagent.get(`https://registry.npmjs.org/${pkg.name}`)
+    .set(userAgent.superagent)
+    .then((res) => {
+      let version;
+      if (pkg.source.tag && res.body['dist-tags'][pkg.source.tag] && res.body.versions[res.body['dist-tags'][pkg.source.tag]]) {
+        version = res.body.versions[res.body['dist-tags'][pkg.source.tag]];
+      } else if (pkg.source.tag) {
         log.disableProgress();
         log.resume();
-        log.error(`no versions match ${pkg.version}`, pkg.name);
+        log.error(`invalid tag ${pkg.source.tag}`, pkg.name);
         log.error('not ok');
         process.exit(1);
+      } else {
+        const versions = Object.keys(res.body.versions)
+          .filter(v => semver.satisfies(v, pkg.version))
+          .sort(semver.compare)
+          .reverse();
+        if (versions.length) {
+          version = res.body.versions[versions[0]];
+        } else {
+          log.disableProgress();
+          log.resume();
+          log.error(`no versions match ${pkg.version}`, pkg.name);
+          log.error('not ok');
+          process.exit(1);
+        }
       }
-    }
 
-    resolve([version, version.dist.tarball, version.dist.shasum]);
-  });
+      resolve([version, version.dist.tarball, version.dist.shasum]);
+    })
+    .catch((res) => {
+      log.disableProgress();
+      log.resume();
+      if (res.status) log.error(`registry error: ${res.status}`, pkg.name);
+      else throw res;
+      log.error('not ok');
+      process.exit(1);
+    });
 });
--- src/misc/userAgent.js
+++ src/misc/userAgent.js
@@ -1,10 +1,8 @@
 const diamondVersion = require('../../package.json').version;
 const superagentVersion = require('superagent/package.json').version;
-const registryVersion = require('npm-registry-client/package.json').version;
 
 module.exports = {
   superagent: {
     'User-Agent': `diamond/v${diamondVersion} superagent/${superagentVersion} node/${process.version}`,
   },
-  registry: `diamond/v${diamondVersion} npm-registry-client/${registryVersion} node/${process.version}`,
 };
