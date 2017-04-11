eda8a0427b739b5a3fe9459c565a8f06f6a74802
Zachary Vacura <zvacura@outlook.com>
2017-04-11 09:53:09 -0500

    fix indentation

--- src/functions/compile/less.js
+++ src/functions/compile/less.js
@@ -10,13 +10,13 @@ module.exports = filename => new Promise((resolve) => {
   if (fs.existsSync('./diamond/.internal/packages.lock')) lockfile.unlockSync('./diamond/.internal/packages.lock');
 
   less.render(fs.readFileSync(filename).toString(), { filename, plugins: [plugin] })
-  .then((result) => {
-    resolve(result.css.toString());
-  }).catch((error) => {
-    log.disableProgress();
-    log.resume();
-    log.error('less', error.message);
-    log.error('not ok');
-    process.exit(1);
-  });
+    .then((result) => {
+      resolve(result.css.toString());
+    }).catch((error) => {
+      log.disableProgress();
+      log.resume();
+      log.error('less', error.message);
+      log.error('not ok');
+      process.exit(1);
+    });
 });
