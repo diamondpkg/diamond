67277b460cbac86d0e8abfd33c445043e46a27bd
Zachary Vacura <zvacura@outlook.com>
2017-04-13 08:59:26 -0500

    better error message

--- src/functions/error.js
+++ src/functions/error.js
@@ -1,6 +1,9 @@
 const os = require('os');
 const osName = require('os-name');
 const raven = require('raven');
+const log = require('npmlog');
+
+log.heading = 'dia';
 
 raven.config('https://b3e855ffb6214bacad64c30ecd947b0b:92764259e54c4501821c339ba95b52af@sentry.io/158014', {
   release: require('../../package.json').version,
@@ -12,6 +15,11 @@ raven.config('https://b3e855ffb6214bacad64c30ecd947b0b:92764259e54c4501821c339ba
     osType: os.platform(),
     nodeVersion: process.version,
   },
-}).install();
+}).install(() => {
+  log.error('internal error', 'this error has been forwarded to the diamond team');
+  log.error('internal error', 'please try again later');
+});
+
+raven.disableConsoleAlerts();
 
 exports = raven;
