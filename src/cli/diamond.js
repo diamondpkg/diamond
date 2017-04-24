7830063b1249aa34238d407ba0b7921f2c9fecf0
Zachary Vacura <zvacura@outlook.com>
2017-04-24 18:52:01 -0500

    Updates

--- src/cli/diamond.js
+++ src/cli/diamond.js
@@ -6,14 +6,19 @@ require('../functions/error');
 
 const program = require('commander');
 
+program.version(require('../../package.json').version);
+
 program
-  .version(require('../../package.json').version)
   .command('install [packages...]', 'install one or more packages')
-  .command('i [packages...]', 'install one or more packages')
-  .command('install [packages...]', 'install one or more packages')
-  .command('i [packages...]', 'install one or more packages')
-  .command('compile <file>', 'compile a Sass file')
-  .command('c <file>', 'compile a Sass file')
-  // .command('login', 'login to GitHub')
-  // .command('config', 'sets the config')
-  .parse(process.argv);
+  .alias('i');
+
+program
+  .command('uninstall [packages...]', 'uninstall one or more packages')
+  .alias('u');
+
+program
+  .command('compile <file>', 'compile a file')
+  .alias('c');
+
+program.parse(process.argv);
+
