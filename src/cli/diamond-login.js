572d8bd50c0a7bc503e897d3542d843a548a902d
Hackzzila <admin@hackzzila.com>
2017-05-23 11:35:15 -0500

    redo login command

--- src/cli/diamond-login.js
+++ src/cli/diamond-login.js
@@ -20,41 +20,58 @@ exports.handler = () => {
   prompt.start();
   prompt.get([
     { name: 'username', required: true },
-    { name: 'email', required: true },
-    { name: 'password', required: true, hidden: true },
-    { name: 'verify password', required: true, hidden: true },
   ], (err, result) => {
     if (err) throw err;
 
-    if (result.password !== result['verify password']) {
-      log.error('passwords do not match');
-      process.exit(1);
-    }
+    superagent.get(`${config.registry}/user/${result.username}`)
+      .then(() => {
+        prompt.get([
+          { name: 'password', required: true, hidden: true },
+        ], (e, resu) => {
+          if (e) throw e;
 
-    superagent.get(`${config.registry}/user`)
-      .auth(result.username, result.password)
-      .then((res) => {
-        const auth = {
-          username: res.body.username, email: res.body.email, password: result.password,
-        };
-        fs.writeFileSync(path.join(os.homedir(), '.diamond/auth.json'), JSON.stringify(auth));
-        log.info('logged in', `as ${res.body.username}`);
-      }).catch((res) => {
-        if (res.status !== 401) throw res;
+          superagent.get(`${config.registry}/user`)
+            .auth(result.username, resu.password)
+            .then((res) => {
+              const auth = {
+                username: res.body.username, email: res.body.email, password: resu.password,
+              };
+              fs.writeFileSync(path.join(os.homedir(), '.diamond/auth.json'), JSON.stringify(auth));
+              log.info('logged in', `as ${res.body.username}`);
+            }).catch((res) => {
+              throw res;
+            });
+        });
+      })
+      .catch((res) => {
+        if (res.status !== 404) throw res;
 
-        superagent.post(`${config.registry}/user`)
-          .send(result)
-          .then((r) => {
-            const auth = {
-              username: r.body.username, email: r.body.email, password: result.password,
-            };
-            fs.writeFileSync(path.join(os.homedir(), '.diamond/auth.json'), JSON.stringify(auth));
-            log.info('registered', `as ${r.body.username}`);
-            log.info('check your email for a verification email');
-          })
-          .catch((e) => {
-            throw e;
-          });
+        prompt.get([
+          { name: 'email', required: true },
+          { name: 'password', required: true, hidden: true },
+          { name: 'verify password', required: true, hidden: true },
+        ], (e, resu) => {
+          if (e) throw e;
+
+          if (resu.password !== resu['verify password']) {
+            log.error('passwords do not match');
+            process.exit(1);
+          }
+
+          superagent.post(`${config.registry}/user`)
+            .send(Object.assign({}, result, resu))
+            .then((r) => {
+              const auth = {
+                username: r.body.username, email: r.body.email, password: resu.password,
+              };
+              fs.writeFileSync(path.join(os.homedir(), '.diamond/auth.json'), JSON.stringify(auth));
+              log.info('registered', `as ${r.body.username}`);
+              log.info('check your email for a verification email');
+            })
+            .catch((r) => {
+              throw r;
+            });
+        });
       });
   });
 };
