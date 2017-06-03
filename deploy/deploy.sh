0152efb763c24ac40385c176cbc84907ff4c3046
Hackzzila <admin@hackzzila.com>
2017-06-02 19:32:30 -0500

    fix build recursion

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,6 +8,11 @@ function doCompile {
   npm run build
 }
 
+if [ "$TRAVIS_COMMIT_MESSAGE" == "Babel Build"* ]; then
+  echo "Ignoring automated build."
+  exit 0
+fi
+
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
--- test/npm.sh
+++ test/npm.sh
@@ -3,6 +3,11 @@
 # Exit on errors
 set -e
 
+if [ "$TRAVIS_COMMIT_MESSAGE" == "Babel Build"* ]; then
+  echo "Ignoring automated build."
+  exit 0
+fi
+
 if [ "$TRAVIS_NODE_VERSION" == "4" ]; then
   printf "Installing Latest npm\n"
   npm i -g npm
