f7349e92a132ab00b85d773b96307c1f933849df
Hackzzila <admin@hackzzila.com>
2017-06-08 20:12:14 -0500

    fix config

--- src/functions/loadConfig.js
+++ src/functions/loadConfig.js
@@ -12,10 +12,11 @@ const defaults = { registry: 'https://registry.diamondpkg.org' };
 
 fs.ensureDirSync(path.join(os.homedir(), '.diamond'));
 if (!fs.existsSync(path.join(os.homedir(), '.diamond/config.yml'))) fs.writeFileSync(path.join(os.homedir(), '.diamond/config.yml'), '');
-let global = yaml.safeLoad(fs.readFileSync(path.join(os.homedir(), '.diamond/config.yml')));
-if (!global) global = {};
+let glo = yaml.safeLoad(fs.readFileSync(path.join(os.homedir(), '.diamond/config.yml')));
+if (!glo) glo = {};
 
 let local = {};
 if (fs.existsSync(path.join(process.cwd(), 'diamond-config.yml'))) local = yaml.safeLoad(fs.readFileSync(path.join(process.cwd(), 'diamond-config.yml')));
 
-module.exports = Object.assign(global, local, defaults);
+module.exports = Object.assign(defaults, glo, local);
+
